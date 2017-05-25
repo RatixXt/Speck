ec232ac54e847d1583e8849b76e6115117e5485b
Shcherbakov Alexey <lovelessxt@gmail.com>
2017-05-25 16:00:54 +0300

    Добавлены функции c ассемблерными вставками
    Добавлены функции speck_setup_asm, speck_encrypt_asm, speck_decrypt_asm, которые являются аналогами speck_setup, speck_encrypt, speck_decrypt с ассемблерными вставками. Внутренние циклы этих функций заменены на ассемблерный код.

--- speck.cpp
+++ speck.cpp
@@ -11,15 +11,15 @@ using namespace std;
 
 extern "C"
 {
-	void speck_round_asm(uint32_t& x_, uint32_t& y_, uint32_t k_);
+	void speck_round_asm(uint32_t& x_, uint32_t& y_, uint32_t& k_);
 }
 
 extern "C"
 {
-	void speck_round_dec_asm(uint32_t& x_, uint32_t& y_, uint32_t k_);
+	void speck_round_dec_asm(uint32_t& x_, uint32_t& y_, uint32_t& k_);
 }
 
-static void speck_round(uint32_t& x_, uint32_t& y_, uint32_t k_)
+static void speck_round(uint32_t& x_, uint32_t& y_, uint32_t& k_)
 {
 	uint8_t a = 8;
 	uint8_t b = 3;
@@ -27,11 +27,12 @@ static void speck_round(uint32_t& x_, uint32_t& y_, uint32_t k_)
 	uint32_t x = x_;
 	uint32_t y = y_;
 	uint32_t k = k_;
+	//printf("0x%08x 0x%08x 0x%08x\n", x, y, k);
 
 	__asm {
 		
 		MOV cl, a
-		ROR x, cl  // x - цикл. сдвиг вправо на альфа
+		ROR x, cl  // x - „€„y„{„|. „ѓ„t„r„y„s „r„Ѓ„‚„p„r„Ђ „~„p „p„|„Ћ„†„p
 
 		MOV eax, x  // x + y
 		ADD eax, y
@@ -40,7 +41,7 @@ static void speck_round(uint32_t& x_, uint32_t& y_, uint32_t k_)
 		MOV x, eax
 		
 		MOV cl, b
-		ROL y, cl // y - цикл. сдвиг влево на бета
+		ROL y, cl // y - „€„y„{„|. „ѓ„t„r„y„s „r„|„u„r„Ђ „~„p „q„u„„„p
 
 		XOR eax, y // y = x XOR y
 		MOV y, eax
@@ -50,7 +51,7 @@ static void speck_round(uint32_t& x_, uint32_t& y_, uint32_t k_)
 	y_ = y;
 }
 
-static void speck_round_dec(uint32_t& x_, uint32_t& y_, const uint32_t k_)
+static void speck_round_dec(uint32_t& x_, uint32_t& y_, uint32_t& k_)
 {
 	uint8_t a = 8;
 	uint8_t b = 3;
@@ -66,7 +67,7 @@ static void speck_round_dec(uint32_t& x_, uint32_t& y_, const uint32_t k_)
 			MOV y, eax
 	
 			MOV cl, b
-			ROR y, cl  // x - цикл. сдвиг вправо на бета
+			ROR y, cl  // x
 
 			MOV eax, x
 			XOR eax, k // x = x XOR k
@@ -75,10 +76,69 @@ static void speck_round_dec(uint32_t& x_, uint32_t& y_, const uint32_t k_)
 			MOV x, eax  
 
 			MOV cl, a
-			ROL x, cl // y - цикл. сдвиг влево на альфа
+			ROL x, cl //
 	}
 	x_ = x;
 	y_ = y;
+}
+
+void speck_setup_asm(const uint32_t key[4]
+	, uint32_t key_schedule[ROUNDS]
+	)
+{
+	uint32_t b = key[0];
+	uint32_t a[3];
+	uint32_t i;
+//	char format[] = "a[i % 3] = 0x%08x b = 0x%08x  i = 0x%08x \n";
+	a[0] = key[1];
+	a[1] = key[2];
+	a[2] = key[3];
+	key_schedule[0] = b;
+	__asm{
+		mov ebx, ROUNDS-1;
+	l1:
+		mov eax, ROUNDS-1;
+		sub eax, ebx;
+		lea ecx, i
+		mov [ecx], eax
+		lea ecx, i;
+		push ecx;
+
+		lea eax, b
+		push eax
+
+		mov eax, ROUNDS-1;
+		sub eax, ebx;
+		xor edx, edx
+		mov ecx, 3
+		div ecx; теперь в edx остаток
+		mov eax, edx
+		imul eax, eax, 4;
+		
+		lea edx, a
+		add eax, edx;
+		push eax;
+
+		call dword ptr speck_round_asm;
+		add esp, 12;
+
+		mov eax, ROUNDS;
+		sub eax, ebx; i + 1
+		imul eax, eax, 4; (i + 1) * 4
+		mov ecx, dword ptr[key_schedule];
+		add eax, ecx
+		mov ecx, b;
+		mov[eax], ecx
+
+		;	 lea eax, format;
+		; push eax;
+		; call dword ptr printf;
+		; add esp, 16;
+
+		sub ebx, 1;
+		jnz l1;
+
+	}
 }
 
 void speck_setup(const uint32_t key[4]
@@ -91,26 +151,77 @@ void speck_setup(const uint32_t key[4]
 	a[1] = key[2];
 	a[2] = key[3];
 	key_schedule[0] = b;
+
 	for (unsigned i = 0; i < ROUNDS - 1; i++) {
+
 		speck_round_asm(a[i % 3], b, i);
 		key_schedule[i + 1] = b;
 	}
+
 }
 
-void speck_encrypt(const uint32_t plaintext[2]
-	, const uint32_t key_schedule[ROUNDS]
+void speck_encrypt_asm(const uint32_t plaintext[2]
+	, uint32_t key_schedule[ROUNDS]
 	, uint32_t ciphertext[2]
 	)
 {
 	ciphertext[0] = plaintext[0];
 	ciphertext[1] = plaintext[1];
+
+	uint32_t a = ciphertext[0];
+	uint32_t b = ciphertext[1];
+
+	char format[] = "ebx = 0x%08x 0x%08x 0x%08x \n";
+	__asm{
+		mov ebx, ROUNDS;
+	l1:
+		mov eax, ROUNDS;
+		sub eax, ebx;
+		mov edx, dword ptr[key_schedule];
+		imul eax, eax, 4;
+		add eax, edx;
+		push eax;
+
+		mov edx, dword ptr[ciphertext];
+		push edx;
+
+		mov edx, dword ptr[ciphertext];
+		add edx, 4;
+		push edx;
+
+		call dword ptr speck_round_asm;
+		add esp, 12;
+
+		;	push ebx;
+		; lea eax, format;
+		; push eax;
+		; call dword ptr printf;
+		; add esp, 16;
+
+		sub ebx, 1;
+		jnz l1;
+
+	}
+}
+
+
+void speck_encrypt(const uint32_t plaintext[2]
+	, uint32_t key_schedule[ROUNDS]
+	, uint32_t ciphertext[2]
+	)
+{
+	ciphertext[0] = plaintext[0];
+	ciphertext[1] = plaintext[1];
+
+	uint32_t a = ciphertext[0];
+	uint32_t b = ciphertext[1];
 	for (unsigned i = 0; i < ROUNDS; i++) {
 		speck_round_asm(ciphertext[1], ciphertext[0], key_schedule[i]);
 	}
 }
 
 void speck_decrypt(const uint32_t ciphertext[2]
-	, const uint32_t key_schedule[ROUNDS]
+	, uint32_t key_schedule[ROUNDS]
 	, uint32_t decrypted[2]
 	)
 {
@@ -119,6 +230,48 @@ void speck_decrypt(const uint32_t ciphertext[2]
 	for (unsigned i = ROUNDS; i > 0; i--) {
 		speck_round_dec_asm(decrypted[1], decrypted[0], key_schedule[i - 1]);
 	}
+}
+
+void speck_decrypt_asm(const uint32_t ciphertext[2]
+	, const uint32_t key_schedule[ROUNDS]
+	, uint32_t decrypted[2]
+	)
+{
+	decrypted[0] = ciphertext[0];
+	decrypted[1] = ciphertext[1];
+
+	char format[] = "dec[1] = 0x%08x dec[0] = 0x%08x  key[i - 1] = 0x%08x \n";
+	__asm{
+		mov ebx, ROUNDS;
+	l1:
+		mov eax, ebx;
+		sub eax, 1;
+		mov edx, dword ptr[key_schedule];
+		imul eax, eax, 4;
+		add eax, edx;
+		push eax;
+
+		mov edx, dword ptr[decrypted];
+		push edx;
+
+		mov edx, dword ptr[decrypted];
+		add edx, 4;
+		push edx;
+
+		call dword ptr speck_round_dec_asm;
+		add esp, 12;
+
+		;	push ebx;
+		; lea eax, format;
+		; push eax;
+		; call dword ptr printf;
+		;  add esp, 16;
+		
+		;--------------------------------------
+		sub ebx, 1;
+		jnz l1;
+
+	} 
 }
 
 
@@ -130,9 +283,9 @@ void test()
 	uint32_t decrypted[2];
 	uint32_t key_schedule[ROUNDS];
 
-	speck_setup(key, key_schedule);
-	speck_encrypt(plaintext, key_schedule, ciphertext);
-	speck_decrypt(ciphertext, key_schedule, decrypted);
+	speck_setup_asm(key, key_schedule);
+	speck_encrypt_asm(plaintext, key_schedule, ciphertext);
+	speck_decrypt_asm(ciphertext, key_schedule, decrypted);
 
 	printf("Plaintext:  0x%08x 0x%08x\n", plaintext[0], plaintext[1]);
 	printf("Key:        0x%08x 0x%08x 0x%08x 0x%08x\n", key[0], key[1], key[2], key[3]);
